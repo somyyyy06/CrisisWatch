46b9f86ca213969f979acc2f60288e252ceeca64
Somya Agarwal <somyaagarwal0608gmail.com>
2026-02-16 04:04:46 +0530

    checking port issue

--- app/database.py
+++ app/database.py
@@ -9,7 +9,7 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 if not DATABASE_URL:
     raise ValueError("DATABASE_URL is not set in the .env file")
 
-engine = create_engine(DATABASE_URL)
+# engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 # Declare Base here
